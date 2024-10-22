a6ba792452b161e4ce0d0c9b2c4b5921a38dddd5
dmi6756@yandex.ru <a0zvx753>
2024-10-22 23:51:23 +0300

    1. Разместа основного экрана. 2.TabLayout создание 3. Доюавление библиотеки "accompanist-pager" для отслеживания состояния кннопок при переключении.

--- app/src/main/java/com/example/weatherapi/MainActivity.kt
+++ app/src/main/java/com/example/weatherapi/MainActivity.kt
@@ -3,7 +3,15 @@ package com.example.weatherapi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.weatherapi.screens.MainScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.weatherapi.screens.MainCard
+import com.example.weatherapi.screens.TabLayout
 import com.example.weatherapi.ui.theme.WeatherApiTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +19,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherApiTheme {
-               MainScreen()
+                Image(
+                    painter = painterResource(id = R.drawable.weather_bg),
+                    contentDescription = "image1",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .alpha(0.8f),
+                    contentScale = ContentScale.FillBounds  // чтобы фон растянулся на весь экран без обрезания
+                )
+                Column {
+                    MainCard()
+                    TabLayout()
+                }
             }
         }
     }
--- app/src/main/java/com/example/weatherapi/screens/MainScreen.kt
+++ app/src/main/java/com/example/weatherapi/screens/MainScreen.kt
@@ -1,24 +1,27 @@
 package com.example.weatherapi.screens
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,22 +30,18 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.weatherapi.R
 import com.example.weatherapi.ui.theme.BlueLight
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
 @Preview(showBackground = true)
 @Composable
-fun MainScreen() {
-    Image(
-        painter = painterResource(id = R.drawable.weather_bg),
-        contentDescription = "image1",
-        modifier = Modifier
-            .fillMaxSize()
-            .alpha(0.5f),
-        contentScale = ContentScale.FillBounds  // чтобы фон растянулся на весь экран без обрезания
-    )
+fun MainCard() {
     //Основной контейнер Column, гед будут карточки и всё остальное
     Column(
         modifier = Modifier
-            .fillMaxSize()
             .padding(5.dp)
     ) {
 
@@ -127,6 +126,60 @@ fun MainScreen() {
                 }
 
             }
+        }
+    }
+}
+
+/**
+ * функция для контейнера с Табами
+ */
+@OptIn(ExperimentalFoundationApi::class, ExperimentalPagerApi::class)
+@Composable
+fun TabLayout() {
+    val tabList = listOf("HOURS", "DAYS")  //список табов (кнопок) для переключения
+    val pagerState = rememberPagerState()  // для сохранения состояния
+    val tabIndex = pagerState.currentPage  // сохранения состояния при запуске, открыта сстраница
+    val coroutineScope = rememberCoroutineScope()  // для запуска анимации переключения
+    Column(
+        modifier = Modifier
+            .padding(
+                start = 5.dp,
+                end = 5.dp
+            )
+            .clip(
+                RoundedCornerShape(5.dp)
+            )
+    ) {
+        TabRow(  //контейнер для кнопок и табов, которые подсвечиваются индликатором. Специальная панелька
+            selectedTabIndex = tabIndex,
+            indicator = { pos ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, pos)
+                )
+            },
+            backgroundColor = BlueLight,
+            contentColor = Color.White
+        ) {
+            tabList.forEachIndexed { index, text ->
+                Tab(
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    text = {
+                        Text(text = text)
+                    }
+                )
+            }
+        }
+        HorizontalPager(
+            count = tabList.size,
+            state = pagerState,
+            modifier = Modifier.weight(1.0f)
+            ) {
+            index ->     // указывает какая страница открыта
         }
     }
 }
